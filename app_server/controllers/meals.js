57f5c229ee2b364f4b4b99455494990d61528075
Jay <jonathan.santiago@snhu.edu>
2023-03-13 14:06:48 -0400

    transfering static data to JSON

--- app_server/controllers/meals.js
+++ app_server/controllers/meals.js
@@ -1,5 +1,9 @@
+const fs = require('fs');
+
+const foods = JSON.parse(fs.readFileSync("./data/meals.json", "utf-8"));
+
 const meals = (req, res) => {
-    res.render('meals', {title: 'Travlr Meals'})
+    res.render('meals', {title: 'Travlr Meals', foods})
 };
 
 module.exports = {
--- app_server/controllers/news.js
+++ app_server/controllers/news.js
@@ -1,5 +1,10 @@
+const fs = require('fs');
+
+const newsArticles = JSON.parse(fs.readFileSync('./data/news.json', 'utf-8'));
+const sideBar = JSON.parse(fs.readFileSync('./data/sidebar.json', 'utf-8'));
+
 const news = (req, res) => {
-    res.render('news', {title: 'Travlr News'})
+    res.render('news', {title: 'Travlr News', newsArticles, sideBar})
 };
 
 module.exports = {
