0fe08d55f5d59233b1e885ab5ffbff79f7488e1a
Jay <jonathan.santiago@snhu.edu>
2023-03-19 22:57:12 -0400

    module 3

--- app_server/controllers/about.js
+++ app_server/controllers/about.js
@@ -1,5 +1,8 @@
+const fs = require('fs')
+const aboutInfo = JSON.parse(fs.readFileSync('./data/about.json', 'utf-8'));
+
 const about = (req, res) => {
-    res.render('about', {title: 'About Travlr'})
+    res.render('about', {title: 'About Travlr', aboutInfo})
 };
 
 module.exports = {
