6531b9d8ec7fd638ba404509a2d22b0524d55f88
Jay <jonathan.santiago@snhu.edu>
2023-03-21 08:50:56 -0400

    finally figured out db problem

--- .seedgooserc.js
+++ .seedgooserc.js
@@ -1,6 +1,6 @@
 module.exports = {
-    modelBaseDirectory: 'models',
-    models: ['*.js', 'db.js'],
-    data: 'data',
-    db: 'mongodb://localhost:27017/travlr'
+    modelBaseDirectory: "app_server/database/models",
+    models: ["*.js", "!db.js"],
+    data: "data",
+    db: "mongodb://127.0.0.1:27017/travlr"
 };
--- app_server/database/db.js
+++ app_server/database/db.js
@@ -1,7 +1,7 @@
 const mongoose = require('mongoose');
 const readLine = require('readline');
 
-let dbURL = 'mongodb://127.0.0.1/travlr';
+let dbURL = "mongodb://127.0.0.1:27017/travlr";
 if (process.env.NODE_ENV === 'production') {
   dbURL = process.env.DB_HOST || process.env.MONGODB_URI;
 }
@@ -58,4 +58,4 @@ process.on('SIGTERM', () => {
 
 connect();
 
-require('./travlr');
+require('./models/travlr');
