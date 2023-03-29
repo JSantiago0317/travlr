4fa2b03c23b7f8cb30f8ae85526255e7b83c21eb
Jay <jonathan.santiago@snhu.edu>
2023-03-29 16:06:23 -0400

    trying to figure out travelDetails

--- app_server/controllers/travel.js
+++ app_server/controllers/travel.js
@@ -1,11 +1,97 @@
-const fs = require('fs');
+const request = require('request');
+const apiOptions = {
+    server: 'http://localhost:3000'
+}
 
-const trips = JSON.parse(fs.readFileSync('./data/trips.json', 'utf-8'));
+/* render travel list view */
+const renderTravelList = (req, res, responseBody) => {
+    let message = null;
+    let pageTitle = process.env.npm_package_description + ' - Travel';
 
-const travel = (req, res) => {
-    res.render('travel', {title: 'Travlr Getaways', trips})
+    if (!(responseBody instanceof Array)) {
+        message = 'API lookup error';
+        responseBody = [];
+    } else {
+        if (!responseBody.length) {
+            message = 'No trips exist in database';
+        }
+    }
+
+    res.render('travel', {
+        title: pageTitle,
+        trips: responseBody,
+        message
+    });
 };
+
+// (I don't know if I'm doing this correctly yet)
+/* render travel details view  */
+// const renderTravelDetails = (req, res, responseBody) => {
+//     let message = null;
+//     let pageTitle = process.env.npm_package_description + ' - Travel Details';
+
+//     if (!(responseBody instanceof Array)) {
+//         message = 'API lookup error';
+//         responseBody = [];
+//     } else {
+//         if (!responseBody.length) {
+//             message = 'No trips exist in database';
+//         }
+//     }
+
+//     res.render('travel', {
+//         title: pageTitle,
+//         trips: responseBody,
+//         message
+//     });
+// };
+
+/* GET travel list view */
+const travelList = (req, res) => {
+    const path = '/api/trips';
+    const requestOptions = {
+        url: `${apiOptions.server}${path}`,
+        method: 'GET',
+        json: {},
+    };
+
+    console.info('>> travelController.travelList calling ' + requestOptions.url);
+
+    request(
+        requestOptions,
+        (err, {statusCode}, body) => {
+            if (err) {
+                console.error(err);
+            }
+            renderTravelList(req, res, body);
+        }
+    )
+}
+
+// (I don't know if I'm doing this correctly yet)
+/* GET travel details view */
+// const travelDetails = (req, res) => {
+//     const path = '/api/trips/:tripCode';
+//     const requestOptions = {
+//         url: `${api.apiOptions.server}${path}`,
+//         method: 'GET',
+//         json: {},
+//     };
+
+//     console.info('>> travelController.travelDetails calling' + requestOptions.url);
+
+//     request(
+//         requestOptions,
+//         (err, {statusCode}, body) => {
+//             if (err) {
+//                 console.error(err);
+//             }
+//             renderTravelDetails(req, res, body);
+//         }
+//     )
+// }
 
 module.exports = {
-    travel
-};
+    travelList,
+    // travelDetails
+}
--- app_server/routes/travel.js
+++ app_server/routes/travel.js
@@ -2,6 +2,7 @@ const express = require('express');
 const router = express.Router();
 const ctrlTravel = require('../controllers/travel');
 
-router.get('/', ctrlTravel.travel);
+router.get('/', ctrlTravel.travelList);
+// router.get('/travel/travelDetails', ctrlTravel.travelDetails); //FIX ME: I have to figure this out
 
 module.exports = router;
