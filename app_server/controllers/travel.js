313cb1c879a13d685a1cb91ab6ba3b00dc116d1e
Jay <jonathan.santiago@snhu.edu>
2023-03-30 17:06:08 -0400

    cleaning up

--- app_server/controllers/travel.js
+++ app_server/controllers/travel.js
@@ -24,28 +24,6 @@ const renderTravelList = (req, res, responseBody) => {
     });
 };
 
-// (I don't know if I'm doing this correctly yet)
-/* render travel details view  */
-// const renderTravelDetails = (req, res, responseBody) => {
-//     let message = null;
-//     let pageTitle = process.env.npm_package_description + ' - Travel Details';
-
-//     if (!(responseBody instanceof Array)) {
-//         message = 'API lookup error';
-//         responseBody = [];
-//     } else {
-//         if (!responseBody.length) {
-//             message = 'No trips exist in database';
-//         }
-//     }
-
-//     res.render('travel', {
-//         title: pageTitle,
-//         trips: responseBody,
-//         message
-//     });
-// };
-
 /* GET travel list view */
 const travelList = (req, res) => {
     const path = '/api/trips';
@@ -68,30 +46,6 @@ const travelList = (req, res) => {
     )
 }
 
-// (I don't know if I'm doing this correctly yet)
-/* GET travel details view */
-// const travelDetails = (req, res) => {
-//     const path = '/api/trips/:tripCode';
-//     const requestOptions = {
-//         url: `${api.apiOptions.server}${path}`,
-//         method: 'GET',
-//         json: {},
-//     };
-
-//     console.info('>> travelController.travelDetails calling' + requestOptions.url);
-
-//     request(
-//         requestOptions,
-//         (err, {statusCode}, body) => {
-//             if (err) {
-//                 console.error(err);
-//             }
-//             renderTravelDetails(req, res, body);
-//         }
-//     )
-// }
-
 module.exports = {
-    travelList,
-    // travelDetails
+    travelList
 }
--- app_server/routes/travel.js
+++ app_server/routes/travel.js
@@ -3,6 +3,5 @@ const router = express.Router();
 const ctrlTravel = require('../controllers/travel');
 
 router.get('/', ctrlTravel.travelList);
-// router.get('/travel/travelDetails', ctrlTravel.travelDetails); //FIX ME: I have to figure this out
 
 module.exports = router;
