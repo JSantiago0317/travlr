1055b164c9f28ec2d5f21d17d8aca2c82af36fe0
Jay <jonathan.santiago@snhu.edu>
2023-04-05 07:43:45 -0400

    fixed description not displaying on new entries

--- app_api/controllers/trips.js
+++ app_api/controllers/trips.js
@@ -62,14 +62,51 @@ const tripAddTrip = async (req, res) => {
                     .json(err);
             } else {
                 return res
-                    .status(200)  //created succesfully
+                    .status(201)  //created succesfully
                     .json(trip);
             }
+        });
+}
+
+const tripsUpdateTrip = async (req, res) => {
+    // console.log(body);
+    model
+        .findOneAndUpdate({ 'code' : req.params.tripCode }, {
+            code: req.body.code,
+            name: req.body.name,
+            length: req.body.length,
+            start: req.body.start,
+            resort: req.body.resort,
+            perPerson: req.body.perPerson,
+            image: req.body.image,
+            description: req.body.description
+        }, { new: true})
+        .then(trip => {
+            if (!trip) {
+                return res
+                    .status(404)
+                    .send({
+                        message: "Trip not found with code " + req.params.tripCode
+                    });
+            }
+            res.send(trip);
+        }).catch(err => {
+            if (err.kind === 'ObjectId') {
+                return res
+                    .status(404)
+                    .send({
+                        message: "Trip not found with code " + req.params.tripCode
+                    });
+            }
+            return res
+                .status(500)  // server error
+                .json(err);
         });
 }
 
 module.exports = {
     tripsList,
     tripsFindCode,
-    tripAddTrip
+    tripAddTrip,
+    tripsUpdateTrip
 };
