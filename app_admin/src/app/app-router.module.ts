9b0151d2aff989b1100c7c13788d088511654d2e
Jay <jonathan.santiago@snhu.edu>
2023-04-14 10:48:33 -0400

    added register user button on log in page

--- app_admin/src/app/app-router.module.ts
+++ app_admin/src/app/app-router.module.ts
@@ -6,6 +6,7 @@ import { EditTripComponent } from "./edit-trip/edit-trip.component";
 import { DeleteTripsComponent } from "./delete-trips/delete-trips.component";
 import { LoginComponent } from "./login/login.component";
 import { HomeComponent } from "./home/home.component";
+import { RegisterUserComponent } from "./register-user/register-user.component";
 
 const routes: Routes = [
     { path: 'add-trip', component: AddTripComponent },
@@ -13,6 +14,7 @@ const routes: Routes = [
     { path: 'delete-trip', component: DeleteTripsComponent },
     { path: 'login', component: LoginComponent},
     { path: 'list-trips', component: TripListingComponent },
+    { path: 'register-user', component: RegisterUserComponent},
     { path: '', component: HomeComponent, pathMatch: 'full'}
 ]
 
--- app_admin/src/app/app.module.ts
+++ app_admin/src/app/app.module.ts
@@ -14,6 +14,7 @@ import { DeleteTripsComponent } from './delete-trips/delete-trips.component';
 import { NavbarComponent } from './navbar/navbar.component';
 import { LoginComponent } from './login/login.component';
 import { HomeComponent } from './home/home.component';
+import { RegisterUserComponent } from './register-user/register-user.component';
 
 @NgModule({
   declarations: [
@@ -25,7 +26,8 @@ import { HomeComponent } from './home/home.component';
     DeleteTripsComponent,
     NavbarComponent,
     LoginComponent,
-    HomeComponent
+    HomeComponent,
+    RegisterUserComponent
   ],
   imports: [
     BrowserModule,
